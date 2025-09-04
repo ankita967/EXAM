d0b2de27af61427cf95da9fd265ae15b6fc63141
Ankita <chakrabortyankita1104@gmail.com>
2025-09-04 20:38:29 +0530

    04-09-2025

---
+++ question4.py
@@ -0,0 +1,4 @@
+s={10,20,30}
+s.add(20)
+print(s)
+print(len(s))
---
+++ question7.py
@@ -0,0 +1,7 @@
+list= [11, 4, 7, 5, 3]
+max_num = list[0]
+
+for i in list:
+    if i > max_num:
+        max_num = i
+print(max_num)
